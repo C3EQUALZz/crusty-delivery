bcbb3e3bc714989c0cd2f671684d8cd5c628f670
matebookWindows <dan.kovalev2013@gmail.com>
2024-09-30 14:09:39 +0300

    added component ExploreMenu.tsx

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.tsx
@@ -0,0 +1,13 @@
+import "./ExploreMenu.css"
+
+export const ExploreMenu = () => {
+    return (
+        <div className="explore-menu">
+            <h1>Explore our menu</h1>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and
+                culinary expertise. Our mission to satisfy your cravings and elevate your dining experience, one
+                delicious meal at a time. </p>
+
+        </div>
+    )
+}
