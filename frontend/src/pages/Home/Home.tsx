69a086a53cb5ea3e5d85f8241e4139bee712dc08
matebookWindows <dan.kovalev2013@gmail.com>
2024-09-30 15:07:32 +0300

    ended with explore menu component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,16 @@
 import "./Home.css"
 import {Header} from "../../components/Header/Header.tsx";
+import {ExploreMenu} from "../../components/ExploreMenu/ExploreMenu.tsx";
+import {useState} from "react";
 
 export const Home = () => {
+
+    const [category, setCategory] = useState("All");
+
     return (
         <div>
             <Header/>
+            <ExploreMenu category={category} setCategory={setCategory}/>
         </div>
     )
 }
