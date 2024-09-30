fb2bdd1383a65defeb37749377ed3ff769046b24
matebookWindows <dan.kovalev2013@gmail.com>
2024-09-30 13:53:05 +0300

    ended with website header

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
 import "./Home.css"
+import {Header} from "../../components/Header/Header.tsx";
 
 export const Home = () => {
     return (
-        <>
-            
-        </>
+        <div>
+            <Header/>
+        </div>
     )
 }
