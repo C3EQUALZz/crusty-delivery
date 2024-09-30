f2e39fa48ce6f08095e1dc98ea61b6808caa4c9b
c3equalz <dan.kovalev2013@gmail.com>
2024-09-30 22:52:03 +0300

    start making footer for my site

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import "./Footer.css"
+import {assets} from "../../assets/assets.ts";
+
+export const Footer = () => {
+    return (
+        <div className="footer" id="footer">
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="logo of our company"/>
+                    <p>Any text for checking</p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="Facebook icon"/>
+                        <img src={assets.twitter_icon} alt="Twitter icon"/>
+                        <img src={assets.linkedin_icon} alt="Linkedin icon"/>
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>Company</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>88005553535</li>
+                        <li>contact@tomato.com</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
