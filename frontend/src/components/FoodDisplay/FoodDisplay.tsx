fa1b6365d1f9b73c3fd03848fb030172442f96b1
matebookWindows <dan.kovalev2013@gmail.com>
2024-09-30 18:15:51 +0300

    added feature for filtering products

--- frontend/src/components/FoodDisplay/FoodDisplay.tsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.tsx
@@ -16,14 +16,16 @@ export const FoodDisplay: React.FC<FoodDisplayProps> = ({category}) => {
             <h2>Top Dishes near you</h2>
             <div className="food-display-list">
                 {food_list.map((item, index) => {
-                    return <FoodItem
-                        key={index}
-                        _id={item._id}
-                        name={item.name}
-                        description={item.description}
-                        price={item.price}
-                        image={item.image}
-                    />
+                    if (category === "All" || item.category === category) {
+                        return <FoodItem
+                            key={index}
+                            _id={item._id}
+                            name={item.name}
+                            description={item.description}
+                            price={item.price}
+                            image={item.image}
+                        />
+                    }
                 })}
             </div>
         </div>
