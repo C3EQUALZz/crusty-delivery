7c68b74e52e16e34a80e88dd1941384ff952ed28
matebookWindows <dan.kovalev2013@gmail.com>
2024-09-30 13:08:23 +0300

    added navigation with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import {Navbar} from "./components/Navbar/Navbar.tsx";
-
+import {Route, Routes} from "react-router-dom"
+import {Home} from "./pages/Home/Home.tsx";
+import {Cart} from "./pages/Cart/Cart.tsx"
+import {PlaceOrder} from "./pages/PlaceOrder/PlaceOrder.tsx";
 
 function App() {
 
-  return (
-    <div className='app'>
-      <Navbar/>
-    </div>
-  )
+    return (
+        <div className='app'>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/cart" element={<Cart/>}/>
+                <Route path="/order" element={<PlaceOrder/>} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
