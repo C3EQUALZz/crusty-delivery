7f38b03183ff127fa6347e4ba3f633c1c9b5e0a5
c3equalz <dan.kovalev2013@gmail.com>
2024-10-10 10:44:04 +0300

    making admin panel

--- admin/src/components/Sidebar/Sidebar.tsx
+++ admin/src/components/Sidebar/Sidebar.tsx
@@ -7,15 +7,15 @@ export const Sidebar = () => {
         <div className="sidebar">
             <div className="sidebar-options">
                 <NavLink to="/add" className="sidebar-option">
-                    <img src={assets.add_icon} alt=""/>
+                    <img src={assets.add_icon} alt="image for button 'add items'"/>
                     <p>Add items</p>
                 </NavLink>
                 <NavLink to="/list" className="sidebar-option">
-                    <img src={assets.order_icon} alt=""/>
+                    <img src={assets.order_icon} alt="image for button 'list items'"/>
                     <p>List items</p>
                 </NavLink>
                 <NavLink to="/orders" className="sidebar-option">
-                    <img src={assets.add_icon} alt=""/>
+                    <img src={assets.add_icon} alt="image for button 'orders'"/>
                     <p>Orders</p>
                 </NavLink>
             </div>
